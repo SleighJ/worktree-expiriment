30883bcd2aa2b3f07ed0d98e0906ef6f0933f62d
JJ Jensen <jonathanjensenjr@gmail.com>
2025-12-11 12:51:36 -0700

    Initial Vite + React setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="app">
+      <h1>Todo App</h1>
+      <p>Base setup - implementations will be in worktree branches</p>
+    </div>
+  )
+}
+
+export default App
+
